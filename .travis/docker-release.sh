c82be89c76fa8bc94ec1daca46bc36e350d85d49
Travis Glenn Hansen <travisghansen@yahoo.com>
2020-02-12 10:52:13 -0700

    per-platform jobs

--- .travis/docker-release.sh
+++ .travis/docker-release.sh
@@ -7,14 +7,14 @@ export DOCKER_PROJECT="democratic-csi"
 export DOCKER_REPO="${DOCKER_ORG}/${DOCKER_PROJECT}"
 
 if [[ -n "${TRAVIS_TAG}" ]]; then
-  docker buildx build --progress plain --pull --push --platform linux/amd64,linux/arm/v7,linux/arm64 -t ${DOCKER_REPO}:${TRAVIS_TAG} .
+  docker buildx build --progress plain --pull --push --platform "${DOCKER_BUILD_PLATFORM}" -t ${DOCKER_REPO}:${TRAVIS_TAG} .
   docker push ${DOCKER_REPO}:${TRAVIS_TAG}
 elif [[ -n "${TRAVIS_BRANCH}" ]]; then
   if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
-    docker buildx build --progress plain --pull --push --platform linux/amd64,linux/arm/v7,linux/arm64 -t ${DOCKER_REPO}:latest .
+    docker buildx build --progress plain --pull --push --platform "${DOCKER_BUILD_PLATFORM}" -t ${DOCKER_REPO}:latest .
     docker push ${DOCKER_REPO}:latest
   else
-    docker buildx build --progress plain --pull --push --platform linux/amd64,linux/arm/v7,linux/arm64 -t ${DOCKER_REPO}:${TRAVIS_BRANCH} .
+    docker buildx build --progress plain --pull --push --platform "${DOCKER_BUILD_PLATFORM}" -t ${DOCKER_REPO}:${TRAVIS_BRANCH} .
     docker push ${DOCKER_REPO}:${TRAVIS_BRANCH}
   fi
 else
