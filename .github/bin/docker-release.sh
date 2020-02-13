3701cb1015ab53eadc25225835eb432a24404624
Travis Glenn Hansen <travisghansen@yahoo.com>
2020-02-12 23:12:37 -0700

    ci in github

---
+++ .github/bin/docker-release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+export DOCKER_ORG="democraticcsi"
+export DOCKER_PROJECT="democratic-csi"
+export DOCKER_REPO="${DOCKER_ORG}/${DOCKER_PROJECT}"
+
+export GIT_BRANCH=${GITHUB_REF#refs/heads/}
+export GIT_TAG=${GITHUB_TAG}
+
+if [[ -n "${GIT_TAG}" ]]; then
+  docker buildx build --progress plain --pull --push --platform "${DOCKER_BUILD_PLATFORM}" -t ${DOCKER_REPO}:${GIT_TAG} .
+elif [[ -n "${GIT_BRANCH}" ]]; then
+  if [[ "${GIT_BRANCH}" == "master" ]]; then
+    docker buildx build --progress plain --pull --push --platform "${DOCKER_BUILD_PLATFORM}" -t ${DOCKER_REPO}:latest .
+  else
+    docker buildx build --progress plain --pull --push --platform "${DOCKER_BUILD_PLATFORM}" -t ${DOCKER_REPO}:${GIT_BRANCH} .
+  fi
+else
+  :
+fi
