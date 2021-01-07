26727620c6f4549d1bddbfdbe6fd3f13233847ad
Travis Glenn Hansen <travisghansen@yahoo.com>
2021-01-06 17:28:13 -0700

    implement crude locks to prevent unwanted concurrency in requests

--- src/utils/general.js
+++ src/utils/general.js
@@ -1,7 +1,37 @@
-function sleep(ms){
-    return new Promise(resolve=>{
-        setTimeout(resolve,ms)
-    })
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+function lockKeysFromRequest(call, serviceMethodName) {
+  switch (serviceMethodName) {
+    // controller
+    case "CreateVolume":
+      return ["create_volume_name_" + call.request.name];
+    case "DeleteVolume":
+    case "ControllerExpandVolume":
+      return ["volume_id_" + call.request.volume_id];
+    case "CreateSnapshot":
+      return [
+        "create_snapshot_name_" + call.request.name,
+        "volume_id_" + call.request.source_volume_id,
+      ];
+    case "DeleteSnapshot":
+      return ["snapshot_id_" + call.request.snapshot_id];
+
+    // node
+    case "NodeStageVolume":
+    case "NodeUnstageVolume":
+    case "NodePublishVolume":
+    case "NodeUnpublishVolume":
+    case "NodeExpandVolume":
+      return ["volume_id_" + call.request.volume_id];
+
+    default:
+      return [];
+  }
 }
 
 module.exports.sleep = sleep;
+module.exports.lockKeysFromRequest = lockKeysFromRequest;
