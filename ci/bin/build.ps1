301af17421ce5bf4d4f19acc00e65c87b699332b
Travis Glenn Hansen <travisghansen@yahoo.com>
2022-05-05 16:00:39 -0600

    add nfs-client and smb-client to test matrix
    Signed-off-by: Travis Glenn Hansen <travisghansen@yahoo.com>

--- ci/bin/build.ps1
+++ ci/bin/build.ps1
@@ -1,8 +1,19 @@
+Write-Output "current user"
+whoami
+Write-Output "current working directory"
+(Get-Location).Path
+Write-Output "current PATH"
+$Env:PATH
+
+Write-Output "node version"
 node --version
+Write-Output "npm version"
 npm --version
 
 # install deps
+Write-Output "running npm i"
 npm i
 
+Write-Output "creating tar.gz"
 # tar node_modules to keep the number of files low to upload
 tar -zcf node_modules-windows-amd64.tar.gz node_modules
