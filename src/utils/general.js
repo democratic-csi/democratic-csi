339e952d1cefe8211d9b6d383cbe2e77bdae4542
Travis Glenn Hansen <travisghansen@yahoo.com>
2023-10-25 15:40:33 -0400

    log truenas request duration
    Signed-off-by: Travis Glenn Hansen <travisghansen@yahoo.com>

--- src/utils/general.js
+++ src/utils/general.js
@@ -2,7 +2,32 @@ const _ = require("lodash");
 const axios = require("axios");
 const crypto = require("crypto");
 const dns = require("dns");
-const crc = require('crc');
+const crc = require("crc");
+
+axios.interceptors.request.use(
+  function (config) {
+    config.metadata = { startTime: new Date() };
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  function (response) {
+    response.config.metadata.endTime = new Date();
+    response.duration =
+      response.config.metadata.endTime - response.config.metadata.startTime;
+    return response;
+  },
+  function (error) {
+    error.config.metadata.endTime = new Date();
+    error.duration =
+      error.config.metadata.endTime - error.config.metadata.startTime;
+    return Promise.reject(error);
+  }
+);
 
 function sleep(ms) {
   return new Promise((resolve) => {
