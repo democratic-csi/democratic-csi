8700b49f6916d92c34526a52cd0ccbbff3c441a5
Michel Peterson <michelp@microsoft.com>
2025-04-05 10:45:38 +0000

    small typo

--- hack/build_push.sh
+++ hack/build_push.sh
@@ -6,7 +6,7 @@ set -x
 ROOT_DIR="$(dirname "$(realpath "$0")")"
 
 GITHUB_USER=${GITHUB_USER:-$(jq -r '.auths."ghcr.io".auth' ~/.docker/config.json|base64 -d|cut -d':' -f1)}
-GITHUB_REPO=${GITHUB_REPO:-$(basename -s ${ROOT_DIR}/../.git $(git remote get-url origin))}
+GITHUB_REPO=${GITHUB_REPO:-$(basename -s .git $(git remote get-url origin))}
 DOCKER_TAG=${DOCKER_TAG:-$(git branch --show-current)-$(git rev-parse --short HEAD)}
 
 if [ -z "${GITHUB_USER}" ]; then
